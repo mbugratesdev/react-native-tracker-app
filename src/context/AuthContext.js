4352ea5f38e711673addb10a0f89e75bbf917ab2
Bugra Ates <mbugratesdev@gmail.com>
2021-03-14 14:28:38 +0300

    Authentication completed

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -7,18 +7,36 @@ const authReducer = (state, action) => {
     switch (action.type) {
         case 'add_error':
             return { ...state, errorMessage: action.payload }
-        case 'signup':
+        case 'signin':
             return { errorMessage: '', token: action.payload }
+        case 'clear_error_message':
+            return { ...state, errorMessage: '' }
+        case 'signout':
+            return { token: null, errorMessage: '' }
         default:
             return state
     }
+}
+
+const tryLocalSignin = (dispatch) => async () => {
+    const token = await AsyncStorage.getItem('token')
+    if (token) {
+        dispatch({ type: 'signin', payload: token })
+        navigate('TrackList')
+    } else {
+        navigate('Signup')
+    }
+}
+
+const clearErrorMessage = (dispatch) => () => {
+    dispatch({ type: 'clear_error_message' })
 }
 
 const signup = (dispatch) => async ({ email, password }) => {
     try {
         const response = await trackerApi.post('/signup', { email, password })
         await AsyncStorage.setItem('token', response.data.token)
-        dispatch({ type: 'signup', payload: response.data.token })
+        dispatch({ type: 'signin', payload: response.data.token })
 
         navigate('TrackList')
     } catch (err) {
@@ -26,22 +44,26 @@ const signup = (dispatch) => async ({ email, password }) => {
     }
 }
 
-const signin = (dispatch) => {
-    return ({ email, password }) => {
-        // try signin
-        // handle success by updating state
-        // handle failure by showing error message (somehow)
+const signin = (dispatch) => async ({ email, password }) => {
+    try {
+        const response = await trackerApi.post('/signin', { email, password })
+        await AsyncStorage.setItem('token', response.data.token)
+        dispatch({ type: 'signin', payload: response.data.token })
+
+        navigate('TrackList')
+    } catch (err) {
+        dispatch({ type: 'add_error', payload: 'Something went wrong with sign in' })
     }
 }
 
-const signout = (dispatch) => {
-    return () => {
-        // somewhat sign out
-    }
+const signout = (dispatch) => async () => {
+    await AsyncStorage.removeItem('token')
+    dispatch({ type: 'signout' })
+    navigate('loginFlow')
 }
 
 export const { Provider, Context } = createDataContext(
     authReducer,
-    { signup, signin, signout },
+    { signup, signin, signout, clearErrorMessage, tryLocalSignin },
     { token: null, errorMessage: '' }
 )
