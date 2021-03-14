cb2144b4b1a70173c616b361a7367f84303f56b5
Bugra Ates <mbugratesdev@gmail.com>
2021-03-14 13:12:51 +0300

    Signup completed with reusable components

---
+++ src/screens/SignupScreen.js
@@ -0,0 +1,33 @@
+import React, { useContext } from 'react'
+import { View, StyleSheet } from 'react-native'
+import { Context as AuthContext } from '../context/AuthContext'
+import AuthForm from '../components/AuthForm'
+import NavLink from '../components/NavLink'
+
+const SignupScreen = () => {
+    const { state, signup } = useContext(AuthContext)
+
+    return (
+        <View style={styles.container}>
+            <AuthForm
+                headerText="Sign Up for Tracker"
+                errorMessage={state.errorMessage}
+                submitButtonText="Sign Up"
+                onSubmit={signup}
+            />
+            <NavLink routeName="Signin" text="Already have an account? Sign in instead" />
+        </View>
+    )
+}
+
+SignupScreen.navigationOptions = { headerShown: false }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        marginBottom: 100,
+    },
+})
+
+export default SignupScreen
